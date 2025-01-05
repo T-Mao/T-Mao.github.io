abfa0a703d2245058b205cef22ee7722b8224d53
T-Mao <86865107+T-Mao@users.noreply.github.com>
2025-01-05 08:19:58 +0000

    Deploying to gh-pages from @ T-Mao/T-Mao.github.io@99e0596e54f667a573895b8da338c912601dde08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -385,51 +385,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-goshsha",
+          title: 'Goshsha',
+          description: "2024/1 – Present",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/Goshsha/";
+            },},{id: "projects-lifetune",
+          title: 'LifeTune',
+          description: "2024/1 – 2024/3",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/LifeTune/";
             },},{id: "projects-mywellness",
           title: 'MyWellness',
           description: "Developed a comprehensive iOS health tracking app in Swift",
           section: "Projects",handler: () => {
               window.location.href = "/projects/MyWellness/";
+            },},{id: "projects-threetodo",
+          title: 'ThreeTodo',
+          description: "2024/4/13 - 2024/4/21",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ThreeTodo/";
+            },},{id: "projects-writercards",
+          title: 'WriterCards',
+          description: "2024/2 - 2024/4",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/WriterCards/";
             },},{
         id: 'social-email',
         title: 'email',
