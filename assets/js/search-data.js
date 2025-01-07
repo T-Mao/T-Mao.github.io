82680a79c4d1c5d6ca9632c9e7f37810b93c8390
T-Mao <86865107+T-Mao@users.noreply.github.com>
2025-01-07 22:07:10 +0000

    Deploying to gh-pages from @ T-Mao/T-Mao.github.io@779d7bcc1cdb1c9ae627a7e58654406898c5f18b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -378,7 +378,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
