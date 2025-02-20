79868abafdc3eb7e9917af1c7b3549fb6071cead
T-Mao <86865107+T-Mao@users.noreply.github.com>
2025-02-20 21:14:09 +0000

    Deploying to gh-pages from @ T-Mao/T-Mao.github.io@5e60d3bd2474ddbfa29c55b671dd67c0273269cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,13 +442,6 @@ ninja.data = [{
           window.open("mailto:%74%6F%6E%67%7A%65%6D%61%6F@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
