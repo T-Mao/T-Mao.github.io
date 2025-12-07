a6d2ff90f3cb2f7ad2c72a8f52207bdadc1269ff
T-Mao <86865107+T-Mao@users.noreply.github.com>
2025-12-07 19:36:42 +0000

    Deploying to gh-pages from @ T-Mao/T-Mao.github.io@7635e88e9dcd184ed3fc1b593ea07ffc676fc6a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -322,16 +322,6 @@ ninja.data = [{
           description: "Future AR Interactive Shopping Experience",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Goshsha/";
-            },},{id: "projects-hd-edu",
-          title: 'HD EDU',
-          description: "New Media &amp; Digital Outreach – My Role as a Content Creator &amp; Marketer",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/HDRedNote/";
-            },},{id: "projects-hd-edu",
-          title: 'HD EDU',
-          description: "My Role as a Calculus Senior Tutor",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/HDTutor/";
             },},{id: "projects-howdidi-app",
           title: 'HowDidI App',
           description: "Paid Résumé-Sharing Platform",
